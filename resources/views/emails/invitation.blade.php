f4f583445434b1f6bb67f14a67921e06b82981e8
HarDToBelieve <tuanit96@gmail.com>
2019-05-29 00:47:27 +0700

    fix bugs mail

--- resources/views/emails/invitation.blade.php
+++ resources/views/emails/invitation.blade.php
@@ -4,6 +4,6 @@
     <title>Even Booking System</title>
 </head>
 <body>
-<strong>Here is your confirm link: </strong> {{$url_mail}}?signup_code={{$signup_code}}
+<strong>Here is your confirm link: </strong> <a href="{{$url_mail}}?signup_code={{$signup_code}}">Confirm link</a>
 </body>
 </html>
